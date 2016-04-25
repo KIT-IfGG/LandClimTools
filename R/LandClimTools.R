530235d4a30172266e1185e586af84d7f6dd6c90
kdolos <dolos@kit.edu>
2016-04-25 17:35:53 +0200

    Added function to complile climate in LandClim format from Profound database.

--- R/LandClimTools.R
+++ R/LandClimTools.R
@@ -182,8 +182,17 @@ create_movie <- function(files, decades, a, species,  scol, plotlegend=T, aspect
 }
 
 
-
-
-
+profound_climate_to_landclim <- function(climate, header, file="climate.txt"){
+  temp <- aggregate(climate$tmean_degC, by=list(mo=climate$mo,year= climate$year), mean)
+  precip <- aggregate(climate$p_mm, by=list(mo=climate$mo, year=climate$year), sum)
+  
+  temp <- round(reshape(temp[,c("year", "mo", "x")],  timevar= "mo", idvar="year", direction = "wide"), 2)
+  precip <- round(reshape(precip[,c("year", "mo", "x")],  timevar= "mo", idvar="year", direction = "wide"), 0)
+  
+  climate <- cbind(temp[,grep("x", names(temp))], precip[,grep("x", names(temp))])
+  
+  writeLines(header, con=file)
+  write.table(climate, file=file, append=TRUE, col.names = FALSE)  
+}
 
 
