1559971036ed6e762210304b24236f015f6fab99
kdolos <dolos@kit.edu>
2016-10-12 09:53:43 +0200

    Enhanced plot_gradient: Did not work for one species. Now it does.

--- R/plot_gradient.R
+++ R/plot_gradient.R
@@ -1,14 +1,20 @@
 plot_gradient <- function(x, y, col=NULL, ...) {
-  y <- t(apply(y, 1, cumsum))
-  if(is.null(col)) col <- rainbow(ncol(y))
+  if(is.null(dim(y))) {
+    y <- cbind(null = rep(0, length(x)), y)
+  } else {
+    y <- t(apply(y, 1, cumsum))
+    y <- cbind(null = rep(0, length(x)), y)
+  }
+  
+  if(is.null(col)) col <- rainbow(ncol(y)-1)
   
   x.poly <- c(x, rev(x))
-  y <- cbind(null= rep(0, length(x)), y)
-  plot(y[,ncol(y)] ~ x, type="n", ...)
   
+  plot(y[,ncol(y)] ~ x, type="n", ...)
   for(i in 2:ncol(y)){
     y.poly <- c(y[,i-1], rev(y[,i]))
     polygon(x.poly, y.poly, col=col[i-1])
   }
   
 }
+
