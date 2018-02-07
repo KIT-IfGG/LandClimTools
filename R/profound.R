0cb8ade0d4f9d7f9c87ba2feffecdfe52aa8b1c7
kdolos <klara.dolos@gmail.com>
2018-02-07 12:42:16 +0100

    shuffle profound climate added to func profound_climate_to_landclim()

--- R/profound.R
+++ R/profound.R
@@ -1,4 +1,4 @@
-profound_climate_to_landclim <- function(climate, header, file="climate.txt"){
+profound_climate_to_landclim <- function(climate, header, file="climate.txt", shuffle = FALSE, n_years=NULL){
   ### Function related to the profound database created in cost action profound 2017. Contact Chr. Reyer, F. Hartig
   ### for access.
   temp <- aggregate(climate$tmean_degC, by=list(mo=climate$mo,year= climate$year), mean)
@@ -8,6 +8,8 @@ profound_climate_to_landclim <- function(climate, header, file="climate.txt"){
   precip <- round(reshape(precip[,c("year", "mo", "x")],  timevar= "mo", idvar="year", direction = "wide"), 0)
   
   climate <- cbind(temp[,grep("x", names(temp))], precip[,grep("x", names(temp))])
+  
+  if(shuffle) climate <- climate[sample(1:nrow(climate), size = n_years, replace=TRUE),]
   
   writeLines(header, con=file)
   write.table(climate, file=file, append=TRUE, col.names = FALSE, quote=FALSE)
